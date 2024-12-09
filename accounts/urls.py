ce450069b5eb98a284ecb6bc069b1b7b77666418
dlfal6338 <shs84956338@gmail.com>
2024-12-09 20:07:25 +0900

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+# accounts/urls.py
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("login2/", views.login2, name="login2"),
+    path("home/", views.home, name="home"),
+    #path("login/", auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+]
